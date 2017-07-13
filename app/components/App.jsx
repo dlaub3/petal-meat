a78a64560c64bb14f22a036d7254fda34d1ca3ff
System <none>
2017-07-13 23:39:40 +0000

    👼🎂 Updated with Glitch

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,20 +3,32 @@
 */
 import React from 'react';
 import Button from './Button.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/RaisedButton';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 export default class App extends React.Component {
   render() {
-    let buttons = this._generateButtons(9);
+    let buttons = this._generateNumButtons(9);
     return (
+    <MuiThemeProvider>
      <div style={{textAlign: 'center'}}>
         <h1>Hello World From React</h1>
-        {buttons}
-    </div>);
+        <div>
+         {buttons}  
+             <RaisedButton label="Secondary" secondary={true} />
+        </div>
+    </div>
+   </MuiThemeProvider>);
   }
-  _generateButtons(n){
+  _generateNumButtons(n){
     let buttons = [];
     for(let i = 0; i <= n; i++) {
-      buttons.push(<Button number={i} className="number"/>);
+      buttons.push(<Button number={i} key={i} className="number"/>);
     }
     return buttons;
   }
